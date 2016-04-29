39afbf353af9a20cdcc248d60efb408294bfc81d
ewei <ewei9046des@hotmail.com>
2016-04-29 16:43:28 +0800

    update dark theme

--- scales/dark.js
+++ scales/dark.js
@@ -3,33 +3,111 @@
 import StyleSheet from '../lib/StyleSheet';
 
 module.exports = {
+
   TView: StyleSheet.create({
     normal: {
-      backgroundColor: 'rgba(0, 0, 0, 0.75)',
+      backgroundColor: '#000',
+      margin: 0,
+      padding: 0
     },
   }),
+
   TText: StyleSheet.create({
     normal: {
-      fontFamily: 'Cochin',
-      fontSize: 20,
-      fontWeight: 'bold',
-      color: 'rgba(255,255,255,1)',
+      fontSize: 14,
+      color: '#77777F',
     },
   }),
+
   TButton: StyleSheet.create({
-    buttonStyle: {
-      backgroundColor: 'red',
+    buttonBox_default: {
+      alignItems: 'center',
+      justifyContent: 'center',
+      height: 36,
+      borderRadius: 2,
+      backgroundColor: '#2D4466',
+      paddingLeft: 16,
+      paddingRight: 16,
+      margin: 4,
+      fontWeight: 'bold',
     },
-    buttonText: {}
+    buttonText_default: {
+      fontSize: 14,
+      color: '#8DACC1',
+      textAlign: 'center',
+    },
+
+    buttonBox_default_disabled: {
+      backgroundColor:'#31353D',
+    },
+    buttonText_default_disabled: {
+      color:'#666',
+    },
+
+    buttonBox_primary:{
+      alignItems: 'center',
+      justifyContent: 'center',
+      height: 36,
+      borderRadius: 2,
+      backgroundColor: '#17A8F9',
+      paddingLeft: 16,
+      paddingRight: 16,
+      margin: 4,
+      fontWeight: 'bold',
+    },
+    buttonText_primary:{
+      fontSize: 14,
+      color: '#000',
+      textAlign: 'center',
+    },
+
+    buttonBox_primary_disabled: {
+      backgroundColor:'#31353D',
+    },
+    buttonText_primary_disabled: {
+      color:'#666',
+    },
+
+    buttonBox_flat: {
+      alignItems: 'center',
+      justifyContent: 'center',
+      height: 36,
+      borderRadius: 2,
+      backgroundColor: 'rgba(0,0,0,0)',
+      paddingLeft: 16,
+      paddingRight: 16,
+      margin: 4,
+      fontWeight: 'bold',
+    },
+    buttonText_flat: {
+      fontSize: 14,
+      color: '#8DACC1',
+      textAlign: 'center',
+    },
+
+    buttonBox_flat_disabled: {
+    },
+    buttonText_flat_disabled: {
+      color:'#666',
+    },
   }),
+
   TInput: StyleSheet.create({
     inputStyle: {
-      borderBottomColor: 'red',
-      height: 36,
-      color:'rgba(255,255,255,1)',
-      borderColor: 'gray',
+      height: 40,
+      borderColor: '#242424',
       borderWidth: 1,
-    },
+      marginTop: 8,
+      marginBottom: 8,
+      marginLeft: 12,
+      marginRight: 12,
+      borderRadius: 2,
+      color: '#949494',
+      backgroundColor: '#242424',
+      paddingLeft: 8,
+      paddingRight: 8,
+      fontSize: 14,
+    }
   })
 };
 
